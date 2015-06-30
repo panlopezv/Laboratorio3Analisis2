0246d57aa13853fd332d6c3cfada8198fb3bf339
EST1522712 <EST1522712@192.168.56.1>
2015-06-30 11:32:44 -0600

    Aguegue Receptores

---
+++ src/laboratorio3/Receptor/Led2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio3.Receptor;
+
+/**
+ *
+ * @author EST1522712
+ */
+public class Led2 implements Leds{
+
+    @Override
+    public void encender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Apagar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
